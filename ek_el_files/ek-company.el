dc03184ceca29b91619a462fb6dc60f3da4c2583
Elliott Krome <tuf78296@temple.edu>
2017-01-23 14:00:16 -0500

    added a little bit of commentary to ek-company.el

--- ek_el_files/ek-company.el
+++ ek_el_files/ek-company.el
@@ -5,20 +5,25 @@
   :diminish company-mode
   :init
   (progn
-    (use-package company-flx
-      :ensure t
-      :defer t)
-    (use-package company-quickhelp
-      :ensure t
-      :init
+
+    (use-package company-flx                   ;;;; fuzzy matching
+      :ensure t                                ;; ensure installation
+      :defer t)                                ;; load lazily
+
+    (use-package company-quickhelp             ;;;; documentation tooltip
+      :ensure t                                ;; ensure installation
+      :init                                    ;; 
       (company-quickhelp-mode 1)
       (setq company-quickhelp-delay 0.9))
-    (global-company-mode))
+
   :config
-  (setq company-idle-delay                0.6
+  (setq company-idle-delay                0.5  ;; shorten
 	company-minimum-prefix-length       1
 	company-echo-delay                  0
 	company-tooltip-limit              20
-	company-tooltip-align-annotations   t)
-  (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
-  (define-key company-active-map (kbd "<backtab>") 'company-select-previous))
+	company-tooltip-align-annotations   t)))
+
+
+(global-company-mode)
+
+
