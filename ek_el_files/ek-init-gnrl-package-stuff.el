b2380bf430e544e215272255885333bcf35e91bf
Elliott Krome <tuf78296@temple.edu>
2017-01-22 20:41:05 -0500

    deleted some redundancy in ek-init-gnrl-package-stuff.el

--- ek_el_files/ek-init-gnrl-package-stuff.el
+++ ek_el_files/ek-init-gnrl-package-stuff.el
@@ -1,9 +1,11 @@
-
+;; speeds up load-time
+;;
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)                ;; if you use :diminish
-(require 'bind-key)                ;; if you use any :bind variant
+(require 'diminish)
 
+;; load packages repos
+;;
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -11,12 +13,11 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
   (package-initialize))
 
+
 ;; http://cachestocaches.com/2015/8/getting-started-use-package/#auto-installing-use-package
+;; use-package cannot load itself, so we will give it some help.
+;; Then it will take care of almost everything else
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
