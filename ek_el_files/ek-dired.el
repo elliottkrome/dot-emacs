8aa8f8724ca8183641d16318196c4087f6064ea3
Elliott Krome <tuf78296@temple.edu>
2017-01-22 20:56:05 -0500

    dired stuff is now in use-package format

--- ek_el_files/ek-dired.el
+++ ek_el_files/ek-dired.el
@@ -1,22 +1,17 @@
 ;;; package --- Summary
 ;;; Commentary:
-;; f7 is bound to toggle omit mode
 ;;; Code:
 
+(use-package dired-x
+  :init
+  (progn
+    (setq-default dired-omit-files-p t)
+    (setq dired-omit-files "^\\.?#\\|^\\.DS_store")
 
-(require 'dired-x)
-(setq-default dired-omit-files-p t)
-(setq dired-omit-files "^\\.?#\\|^\\.DS_store")
-(eval-after-load 'dired
-  '(define-key dired-mode-map [f7] 'dired-omit-mode))
+    (use-package ls-lisp
+    :init
+    (progn
+      (setq ls-lisp-use-insert-directory-program nil)
+      (setq ls-lisp-verbosity '(links))))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; make dired less verbose
-(require 'ls-lisp)
-(setq ls-lisp-use-insert-directory-program nil)
-(setq ls-lisp-verbosity '(links))
-;; end make dired less verbose? ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(provide 'dired)
 ;;; dired.el ends here
