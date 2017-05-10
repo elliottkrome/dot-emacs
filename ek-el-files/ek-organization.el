49a7cb8dc292ea9cff1f154e1a526598536fb914
Elliott Krome <tuf78296@temple.edu>
2017-05-09 21:41:07 -0400

    cleaned up a ek-organization.el a little bit

--- ek-el-files/ek-organization.el
+++ ek-el-files/ek-organization.el
@@ -1,17 +1,16 @@
+;; having a bunch of unnecesary stuff user-emacs-directory makes my
+;; ocd bell start ringing. lets make a place to hide this stuff away
+;;
 (setq ek-hidden-directory
-      (concat user-emacs-directory
-	      (convert-standard-filename "hidden/")))
-
+      (concat user-emacs-directory "hidden/"))
 (setq ek-data-directory
-      (concat ek-hidden-directory
-	      (convert-standard-filename "data/")))
+      (concat ek-hidden-directory "data/"))
 
-
+;; now lets do the actual hiding of those pesky little things
+;;
 (setq bookmark-default-file
       (concat ek-data-directory "bookmarks"))
-
 (setq ac-comphist-file
       (concat ek-data-directory "ac-comphist.dat"))
-
 (setq recentf-save-file
       (concat ek-data-directory "recentf"))
