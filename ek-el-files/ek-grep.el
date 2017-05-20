79013bf97aeea5f72ae910c109700b0d4a1c6c1c
Elliott Krome <tuf78296@temple.edu>
2017-05-20 10:40:05 -0500

    consolidated grep

--- ek-el-files/ek-grep.el
+++ ek-el-files/ek-grep.el
@@ -1,5 +1,13 @@
+(require 'grep)
+
 (eval-after-load "grep"
   '(progn
     (add-to-list 'grep-find-ignored-files "*.pdf")
     (add-to-list 'grep-find-ignored-directories "elpa/archives")
     (add-to-list 'grep-find-ignored-directories "elpa/pkg-info*")))
+
+
+;; editable grep buffer (!)
+(use-package wgrep
+  :commands (wgrep-change-to-wgrep-mode)
+  :ensure t)
--- ek-el-files/ek-wgrep.el
+++
@@ -1,3 +0,0 @@
-(use-package wgrep
-  :commands (wgrep-change-to-wgrep-mode)
-  :ensure t)
