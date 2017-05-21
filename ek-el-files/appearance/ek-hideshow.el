2b9877db59eeded7f239197c51d2b9761bd15d89
Elliott Krome <tuf78296@temple.edu>
2017-05-21 17:35:58 -0500

    added attempt to get hs to hide / show python class methods

--- ek-el-files/appearance/ek-hideshow.el
+++ ek-el-files/appearance/ek-hideshow.el
@@ -12,3 +12,21 @@
     ;; graphical +/- fold buttons
     (hideshowvis-symbols)))
 
+;; unsuccesful attempt to get python Class.method_name folding
+;;
+;; (setq hs-level 2)
+;; (setq hs-fold-show-only-methods-active-p nil)
+;;   (defun hs-fold-show-only-methods ()
+;;     "Toggle between hiding all methods using `hs-level' or
+;; showing them."
+;;     (interactive)
+;;     (save-excursion
+;;       (if hs-fold-show-only-methods-active-p
+;;           (progn
+;;             (hs-show-all)
+;;             (setq-local hs-fold-show-only-methods-active-p t))
+;;         (progn
+;;           (goto-char (point-min))
+;;           (hs-hide-level hs-level)
+;;           (setq-local hs-fold-show-only-methods-active-p t)))))
+
