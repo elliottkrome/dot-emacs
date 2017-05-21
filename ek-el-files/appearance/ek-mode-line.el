e5545ee85a0ddabc195bd796570e66dcf60c17e4
Elliott Krome <tuf78296@temple.edu>
2017-05-21 16:11:02 -0500

    diminished hs-minor-mode

--- ek-el-files/appearance/ek-mode-line.el
+++ ek-el-files/appearance/ek-mode-line.el
@@ -11,7 +11,11 @@
 (diminish 'which-key-mode)
 (diminish 'auto-revert-mode)
 (diminish 'anaconda-mode)
+(diminish 'hs-minor-mode)
 
 ;; Remove BufFace from mode line
 (eval-after-load "face-remap"
   '(diminish 'buffer-face-mode))
+
+
+
