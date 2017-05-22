48c5ad1b86c8772899e6e9680344620f8eeb04c9
Elliott Krome <tuf78296@temple.edu>
2017-05-22 00:24:52 -0500

    moved stuff out of custom

--- ek-el-files/appearance/ek-theme.el
+++ ek-el-files/appearance/ek-theme.el
@@ -8,3 +8,7 @@
 
 (set-face-background 'isearch "coral4")
 (set-face-attribute 'font-lock-comment-face nil :slant 'normal)
+
+(set-face-attribute 'org-level-1 nil :height 1.3)
+(set-face-attribute 'org-level-2 nil :height 1.15)
+ 
--- ek-el-files/ek-custom.el
+++ ek-el-files/ek-custom.el
@@ -5,14 +5,5 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-download hideshowvis yasnippet spaceline wgrep git-gutter-fringe suggest ox-latex ls-lisp flycheck-pyflakes flycheck markdown-mode rainbow-delimiters-mode t rainbow-delimiters iedit ido-occur company-quickhelp company-flx magit use-package)))
- '(safe-local-variable-values
-   (quote
-    ((org-download-image-dir . "~/ddrop/org/knowledge/resources/gnrl_tools/emacs_pictures/")))))
+    (org-download hideshowvis yasnippet spaceline wgrep git-gutter-fringe suggest ox-latex ls-lisp flycheck-pyflakes flycheck markdown-mode rainbow-delimiters-mode t rainbow-delimiters iedit ido-occur company-quickhelp company-flx magit use-package))))
  
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
