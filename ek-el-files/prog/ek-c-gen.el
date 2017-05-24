9220733de6666e7cea556f396a1d68028a1eb5b7
Elliott Krome <tuf78296@temple.edu>
2017-05-23 21:28:53 -0500

    added cmake-ide

--- ek-el-files/prog/ek-c-gen.el
+++ ek-el-files/prog/ek-c-gen.el
@@ -1,34 +1,48 @@
-(setq clang-executable "usr/bin/clang++")
+;; http://parbo.github.io/blog/2016/05/10/configuring-emacs-for-cpp/
 
-;; (add-to-list 'load-path
-;; 	     (expand-file-name "clangcheck.el" 'user-emacs-directory )) ;; if you installed manually
+(use-package irony
+  :ensure t
+  :config
+  (use-package company-irony
+    :ensure t
+    :config
+    (add-to-list 'company-backends 'company-irony))
+  (use-package company-irony-c-headers
+    :ensure t
+    :config
+    (add-to-list 'company-backends 'company-irony-c-headers))
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+  ;; replace the `completion-at-point' and `complete-symbol' bindings in
+  ;; irony-mode's buffers by irony-mode's function
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
 
-;; (defun my-select-clangcheck-for-checker ()
-;;   "Select clang-check for flycheck's checker."
-;;   (flycheck-set-checker-executable 'c/c++-clangcheck
-;;                                    "/path/to/clang-check")
-;;   (flycheck-select-checker 'c/c++-clangcheck))
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode)
+  :bind (("<backtab>" . company-complete-common-or-cycle))
+  :config
+  (delete 'company-backends 'company-clang))
 
-;; (add-hook 'c-mode-hook #'my-select-clangcheck-for-checker)
-;; (add-hook 'c++-mode-hook #'my-select-clangcheck-for-checker)
-
-;; ;; enable static analysis
-;; (setq flycheck-clangcheck-analyze t)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; https://github.com/coldnew/coldnew-emacs
-(setq qt-base-directory "~/Qt")
-(setq qt-include-directory
-      (expand-file-name "5.8/clang_64/lib/QtCore.framework/Headers"
-			qt-base-directory))
-
-(let ((flycheck-qt-include-path-prefix qt-include-directory))
-  (when (file-exists-p flycheck-qt-include-path-prefix)
-      (setq flycheck-clang-include-path
-            (append
-             (mapcar (lambda (include-dirs) (concat "" include-dirs))
-                     (directory-files flycheck-qt-include-path-prefix t "\\w+"))
-             (split-string flycheck-qt-include-path-prefix)))
-      (setq flycheck-clang-custom-flags (split-string "-fPIC"))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+(use-package cmake-ide
+  :ensure t
+  :init
+  (use-package semantic/bovine/gcc)
+  (setq cmake-ide-flags-c++
+	(append '("-std=c++11")
+		(mapcar
+		 (lambda (path) (concat "-I" path))
+		 (semantic-gcc-get-include-paths "c++"))))
+  (setq cmake-ide-flags-c
+	(append
+	 (mapcar (lambda (path) (concat "-I" path))
+		 (semantic-gcc-get-include-paths "c"))))
+  (cmake-ide-setup))
