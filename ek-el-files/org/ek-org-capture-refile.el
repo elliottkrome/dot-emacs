8407f71ed2ef3defd312c43561c3e1dc471c4ed2
Elliott Krome <tuf78296@temple.edu>
2017-05-28 22:49:43 -0500

    removed unnecessary stuff from ek-org-capture-refile.el

--- ek-el-files/org/ek-org-capture-refile.el
+++ ek-el-files/org/ek-org-capture-refile.el
@@ -1,16 +1,6 @@
 
 (require 'org)
 
-;; refile to opened orgmode buffer
-;;
-(defun org-buffer-files ()
-  "Return list of opened orgmode buffer files."
-  (mapcar (function buffer-file-name)
-	  (org-buffer-list 'files)))
-
-(setq org-refile-targets (quote ((nil :maxlevel . 4)
-                                 (org-agenda-files :maxlevel . 4))))
-
 ; Use full outline paths for refile targets - we file directly with IDO
 (setq org-refile-use-outline-path t)
 
@@ -21,8 +11,8 @@
 (setq org-refile-allow-creating-parent-nodes (quote confirm))
 
 ; Exclude DONE state tasks from refile targets
-(defun verify-refile-target ()
-  "Exclude todo keywords with a done state from refile targets."
-  (not (member (nth 2 (org-heading-components)) org-done-keywords)))
-(setq org-refile-target-verify-function 'verify-refile-target)
+;; (defun verify-refile-target ()
+;;   "Exclude todo keywords with a done state from refile targets."
+;;   (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+;; (setq org-refile-target-verify-function 'verify-refile-target)
 
