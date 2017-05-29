c75a05df25fbf39060f31cd4d8635f6f88127ef1
Elliott Krome <tuf78296@temple.edu>
2017-05-29 18:56:16 -0500

    better org-drill config

--- ek-el-files/org/ek-org-drill.el
+++ ek-el-files/org/ek-org-drill.el
@@ -1,6 +1,20 @@
-(require 'org-drill)
-
-(setq org-drill-learn-fraction 0.25
-      org-drill-adjust-intervals-for-early-and-late-repetitions-p t
-      ; org-drill-add-random-noise-to-intervals-p 1
-      org-drill-save-buffers-after-drill-sessions-p nil)
+(use-package org-drill
+  :defer t
+  :commands (org-drill
+             org-drill-tree
+             org-drill-directory)
+  :init
+  (setq org-drill-maximum-items-per-session         50
+        org-drill-maximum-duration                  20   ; 20 minutes
+        org-drill-use-visible-cloze-face-p           t
+        org-drill-add-random-noise-to-intervals-p    t
+        org-drill-hint-separator                  "||"
+        org-drill-left-cloze-delimiter            "<["
+        org-drill-right-cloze-delimiter           "]>"
+        ; org-drill-learn-fraction                0.25
+        ; org-drill-cram-hours 2
+        ; org-drill-leech-method 'warn
+	)
+  :config
+  (progn
+    (add-to-list 'org-modules 'org-drill)))
