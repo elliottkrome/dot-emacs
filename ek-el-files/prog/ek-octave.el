635aefd188d532ad5f595249785d1657e5d54d97
Elliott Krome <tuf78296@temple.edu>
2017-06-08 20:39:39 -0500

    moved stuff from custom into octave

--- ek-el-files/prog/ek-octave.el
+++ ek-el-files/prog/ek-octave.el
@@ -1,27 +1,38 @@
 (use-package octave
   :ensure t
   :mode "\\.m\\'"
-
-  :init
+  :config
   (progn
-    (if (not window-system)
-        (progn
-          (company-quickhelp-mode 0)
-          ))
-    (setq auto-mode-alist
-      (cons '("\\.m$" . octave-mode) auto-mode-alist))
+    (bind-keys :map octave-mode-map
+	       ("C-c ;" . iedit-mode))
+    (setq inferior-octave-buffer       "*Octave*"
+	  inferior-octave-mode-hook    nil
+	  inferior-octave-program      "/usr/local/bin/octave-cli-4.2.1"
+	  inferior-octave-startup-args '("-i" "--no-line-editing" "--no-gui")
+	  octave-block-offset          2)
 
+    ;; company-quickelp mode seems to cause problems in the terminal
+    ;;
+    (if (not window-system)
+	(company-quickhelp-mode 0))
+
+    ;; open .m files in octave mode
+    ;; 
+    (setq auto-mode-alist (cons '("\\.m$" . octave-mode) auto-mode-alist))
+
+    ;; create an octave backend function for company
+    ;;
     (defun company-octave (command &optional arg &rest args)
       "`company-mode' backend using `completion-at-point-functions'."
       (interactive (list 'interactive))
       (if (eq command 'doc-buffer)
-          ;; TODO `octave-help' displays the help buffer, we'd rather use a function
-          ;; that doesn't display the help buffer
           (save-window-excursion
             (ignore-errors
               (octave-help arg)
               (get-buffer octave-help-buffer)))
         (apply #'company-capf command arg args)))
+
+    ;; create a hood to add to octave-mode hook
     (defun my-octave-mode-hook ()
       (progn
 	(add-to-list 'company-backends 'company-octave)
@@ -31,13 +42,11 @@
             (if (eq window-system 'x)
                 (font-lock-mode 1)))))
 
-    (add-hook 'octave-mode-hook 'my-octave-mode-hook)
-    )
-  :config
-  (bind-keys :map octave-mode-map
-	     ("C-c ;" . iedit-mode))
-  )
+    ;; add customized hook to octave-mode-hook
+    ;;
+    (add-hook 'octave-mode-hook 'my-octave-mode-hook))
+ )
 
 
 
-(autoload 'octave-mode "octave-mod" nil t)
+; (autoload 'octave-mode "octave-mod" nil t)
