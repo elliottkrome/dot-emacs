87ae3c5f7c5a13819ca8677e5350972ab909bb3d
Elliott Krome <tuf78296@temple.edu>
2017-07-16 00:32:20 -0500

    added advice to org-download to clean sub-header-strings

--- ek-el-files/org/ek-org-download.el
+++ ek-el-files/org/ek-org-download.el
@@ -2,4 +2,21 @@
   :ensure t
   :after org
   :config
-  (setq org-download-method 'directory))
+  (setq org-download-method 'directory)
+
+  ;; org-download seems to have a problem with whitespace
+  ;; (and maybe angle-brackets (?))in sub-header strings.
+  ;; This function advises an internal function to clean these strings
+  ;;
+  (defun ek-clean-org-directory (orig-val)
+    "Remove angle brackets and replace whitespace with underscores. Used to advise `org-download--dir-2'."
+    (replace-regexp-in-string
+     ">" ""
+     (replace-regexp-in-string
+      "<" ""
+      (replace-regexp-in-string
+       " " "_" orig-val))))
+
+  ;; advise the internal function
+  ;;
+  (advice-add 'org-download--dir-2 :filter-return #'ek-clean-org-directory))
