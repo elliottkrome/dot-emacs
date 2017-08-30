7a7cfba1a5f57b878ab314128609adba1d465d1e
Elliott Krome <tuf78296@temple.edu>
2017-08-30 19:13:33 -0400

    added dired-jump keybinding to ctrl-l map

--- ek-el-files/org/ek-org-agenda.el
+++ ek-el-files/org/ek-org-agenda.el
@@ -6,7 +6,7 @@
 (require 'org-agenda)
 
 (defvar ek-org-agenda-face-height 1.2 "Size for org agenda faces.")
-(defvar ek-num-days-agenda 14 "Number of days in agenda view")
+(defvar ek-num-days-agenda 10 "Number of days in agenda view")
 
 ;; Use sticky agenda's so they persist
 (setq org-agenda-sticky t)
