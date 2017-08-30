6a5b4c7a37f294c559251789d8fd2c11d44ca9f7
Elliott Krome <tuf78296@temple.edu>
2017-08-30 13:59:52 -0400

    added ek-matlab.el

--- ek-el-files/ek-prog.el
+++ ek-el-files/ek-prog.el
@@ -2,5 +2,6 @@
 (load "~/.emacs.d/ek-el-files/prog/ek-markdown.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-python.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-octave.el")
+(load "~/.emacs.d/ek-el-files/prog/ek-matlab.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-cmake.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-clisp.el")
