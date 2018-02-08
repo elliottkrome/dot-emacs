09c0c1a0db6041cebb74eee342cd7046e1c621a6
Elliott Krome <tuf78296@temple.edu>
2018-02-08 18:17:18 -0500

    simplified mode line

--- ek-el-files/appearance/ek-mode-line.el
+++ ek-el-files/appearance/ek-mode-line.el
@@ -10,6 +10,22 @@
 (diminish 'auto-revert-mode)
 (diminish 'anaconda-mode)
 (diminish 'hs-minor-mode)
+(setq mode-line-format
+      '("%e"
+	; mode-line-front-space
+	; mode-line-mule-info
+	; mode-line-client
+	mode-line-modified
+	mode-line-remote
+	mode-line-frame-identification
+	mode-line-buffer-identification
+	"   "
+	mode-line-position
+	(vc-mode vc-mode)
+	"  "
+	; mode-line-modes
+	; mode-line-misc-info
+	mode-line-end-spaces))
 
 ;; Remove BufFace from mode line
 ;;
