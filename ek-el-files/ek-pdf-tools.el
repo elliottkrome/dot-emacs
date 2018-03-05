450946f36d6936ee49160c44a4ddcc348eecb842
Elliott Krome <tuf78296@temple.edu>
2018-03-04 19:12:16 -0500

    fixed pdf-tools

--- ek-el-files/ek-pdf-tools.el
+++ ek-el-files/ek-pdf-tools.el
@@ -1,37 +1,37 @@
-(setenv "PKG_CONFIG_PATH"
-	(concat
-	 "/usr/local/Cellar/zlib/1.2.8/lib/pkgconfig" ":"
-	 "/usr/local/lib/pkgconfig"                   ":"
-	 "/opt/X11/lib/pkgconfig"
-	 (getenv "PKG_CONFIG_PATH")
-	 )
-	)
-(use-package tablist
-  :ensure t)
+;; from https://emacs.stackexchange.com/questions/13314/install-pdf-tools-on-emacs-macosx
+;;
+;; Install epdfinfo via 'brew install pdf-tools' and then install the pdf-tools
+;; elisp via the use-package below. To upgrade the epdfinfo server, just do
+;; 'brew upgrade pdf-tools' prior to upgrading to newest pdf-tools package
+;; using Emacs package system. If things get messed up, just do 'brew uninstall
+;; pdf-tools', wipe out the elpa pdf-tools package and reinstall both as at the
+;; start.
 (use-package pdf-tools
   :ensure t
-  :defer t
-   :config
-    (pdf-tools-install)
-    (setq-default pdf-view-display-size 'fit-page)
-    (bind-keys :map pdf-view-mode-map
-        ("<s-spc>" .  pdf-view-scroll-down-or-next-page)
-        ("g"  . pdf-view-first-page)
-        ("G"  . pdf-view-last-page)
-        ("l"  . image-forward-hscroll)
-        ("h"  . image-backward-hscroll)
-        ("j"  . pdf-view-next-page)
-        ("k"  . pdf-view-previous-page)
-        ("e"  . pdf-view-goto-page)
-        ("u"  . pdf-view-revert-buffer)
-        ("al" . pdf-annot-list-annotations)
-        ("ad" . pdf-annot-delete)
-        ("aa" . pdf-annot-attachment-dired)
-        ("am" . pdf-annot-add-markup-annotation)
-        ("at" . pdf-annot-add-text-annotation)
-        ("y"  . pdf-view-kill-ring-save)
-        ("i"  . pdf-misc-display-metadata)
-        ("s"  . pdf-occur)
-        ("b"  . pdf-view-set-slice-from-bounding-box)
-        ("r"  . pdf-view-reset-slice)))
+  :config
+  (custom-set-variables
+   '(pdf-tools-handle-upgrades nil)) ; Use brew upgrade pdf-tools instead.
+  (setq pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo")
+  (bind-keys :map pdf-view-mode-map
+	     ("<s-spc>" .  pdf-view-scroll-down-or-next-page)
+	     ("g"  . pdf-view-first-page)
+	     ("G"  . pdf-view-last-page)
+	     ("l"  . image-forward-hscroll)
+	     ("h"  . image-backward-hscroll)
+	     ("j"  . pdf-view-next-page)
+	     ("k"  . pdf-view-previous-page)
+	     ("e"  . pdf-view-goto-page)
+	     ("u"  . pdf-view-revert-buffer)
+	     ("al" . pdf-annot-list-annotations)
+	     ("ad" . pdf-annot-delete)
+	     ("aa" . pdf-annot-attachment-dired)
+	     ("am" . pdf-annot-add-markup-annotation)
+	     ("at" . pdf-annot-add-text-annotation)
+	     ("y"  . pdf-view-kill-ring-save)
+	     ("i"  . pdf-misc-display-metadata)
+	     ("s"  . pdf-occur)
+	     ("b"  . pdf-view-set-slice-from-bounding-box)
+	     ("r"  . pdf-view-reset-slice)))
+
+(pdf-tools-install)
 
