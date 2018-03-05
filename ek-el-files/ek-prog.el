282f6fdc9e9807107706f774485f96f0d94136ec
Elliott Krome <tuf78296@temple.edu>
2018-03-04 19:12:29 -0500

    removed ob-spice from loading

--- ek-el-files/ek-prog.el
+++ ek-el-files/ek-prog.el
@@ -6,5 +6,5 @@
 (load "~/.emacs.d/ek-el-files/prog/ek-cmake.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-clisp.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-gnuplot.el")
-(load "~/.emacs.d/ek-el-files/prog/ek-spice.el")
+; (load "~/.emacs.d/ek-el-files/prog/ek-spice.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-scad.el")
