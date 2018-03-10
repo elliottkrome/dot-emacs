afd29cdf48e68d59249d96d4e91f80dd8e13455e
Elliott Krome <tuf78296@temple.edu>
2018-03-09 22:10:42 -0500

    why was org-latex-pdf-process calling the process twice?

--- ek-el-files/org/ek-org-export.el
+++ ek-el-files/org/ek-org-export.el
@@ -14,9 +14,7 @@
 (add-to-list 'org-latex-packages-alist '("" "minted"))
 (setq org-latex-listings 'minted) 
 (setq org-latex-pdf-process
-      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 
  (setq org-latex-minted-options
        '(("frame" "lines")
