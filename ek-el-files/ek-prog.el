a60a212408ff7b7cd736f78ff0eabd6ce4326790
Elliott Krome <tuf78296@temple.edu>
2018-03-21 15:01:50 -0400

    moved org-pdfview into own file

--- ek-el-files/ek-prog.el
+++ ek-el-files/ek-prog.el
@@ -6,5 +6,6 @@
 (load "~/.emacs.d/ek-el-files/prog/ek-cmake.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-clisp.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-gnuplot.el")
+(load "~/.emacs.d/ek-el-files/prog/ek-ruby.el")
 ; (load "~/.emacs.d/ek-el-files/prog/ek-spice.el")
 (load "~/.emacs.d/ek-el-files/prog/ek-scad.el")
