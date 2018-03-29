10f971d4b10eb855fab133ea4af62c7a9096abec
Elliott Krome <tuf78296@temple.edu>
2018-03-28 22:38:04 -0400

    random window stuff

--- ek-el-files/appearance/ek-window-behavior.el
+++ ek-el-files/appearance/ek-window-behavior.el
@@ -1,39 +1,41 @@
 
-(setq frame-title-format '("%b")) ;; display buffer name in title-bar
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-(toggle-scroll-bar 0)
-(global-font-lock-mode 't)
-(winner-mode 1)
-(column-number-mode 1)      ;; display column number in mode line
-(blink-cursor-mode 0)
-(show-paren-mode t)
+(tool-bar-mode 0)                ;; no tool-bar
+(scroll-bar-mode 0)              ;; no scroll-bar
+(toggle-scroll-bar 0)            ;;  ||
+(winner-mode 1)                  ;; make moving between frame layouts easy
+(column-number-mode 1)           ;; display column number in mode line
+(blink-cursor-mode 0)            ;; no blinky cursor
+(show-paren-mode t)              ;; tell (message "message" format-args)e about partners
+(fringe-mode '(0 . 0))           ;; 0 pixels on the left, 0 pixels on the right
 
+(setq frame-title-format '("%b") ;; display buffer name in title-bar
+      echo-keystrokes 0.05       ;; show unfinished keystrokes early.
+      visible-bell t             ;; dinging noises annoy the people around me
+      scroll-conservatively 1000 ;; scroll just enough to bring point into view
+      inhibit-splash-screen t)   ;; no splash screen
 
-;; 8 pixels on the left, 0 pixels on the right
+;; buffer-local variable, so must use `setq-default', rather then `setq'
 ;;
-(fringe-mode '(0 . 0))
-
-(setq
- echo-keystrokes 0.05       ;; show unfinished keystrokes early.
- visible-bell t             ;; dinging noises annoy the people around me
- scroll-conservatively 1000 ;; scroll just enough to bring point into view
- inhibit-splash-screen t    ;; no splash screen
- )
-
 (setq-default truncate-lines t)
-;; (setq
-;;  truncate-lines t                 ;; each line of text gets one line on the
-;; 				     ;; screen (i.e., text will run off the left
-;; 				     ;; instead of wrapping around onto a new line)
-;;  truncate-partial-width-windows t ;; truncate lines in partial-width windows
-;;  )
 
-;; (defun highlight-selected-window ()
-;;   "Highlight un-selected windows with a different background color."
-;;   (walk-windows (lambda (w)
-;; 		  (unless (eq w (selected-window))
-;;                     (with-current-buffer (window-buffer w)
-;; 		      (buffer-face-set '(:background "#000e26"))))))
-;;   (buffer-face-set 'default))
-;; (add-hook 'buffer-list-update-hook 'highlight-selected-window)
+;; (not a great use of time)
+;; aesthetics - just playing with some frame appearance stuff
+;; 
+(if (eq system-type 'darwin)
+    (setq initial-frame-alist `((scroll-bar-background . "PaleVioletRed3") ; "gray60" works well with dark-mode on mac
+				(                alpha . (100 . 100))
+				(     scroll-bar-width . 0))
+	  default-frame-alist (copy-alist initial-frame-alist)))
+
+;; i think this was the first non-trivial elisp i ever messed around
+;; with. i'm keeping it around for nostalgia's sake.
+;;
+(defun highlight-selected-window ()
+  "Highlight un-selected windows with a different background color."
+  (walk-windows (lambda (w)
+		  (if (not (eq w (minibuffer-window)))
+		      (unless (eq w (selected-window))
+			(with-current-buffer (window-buffer w)
+			  (buffer-face-set '(:background "#26322e")))))))
+  (buffer-face-set 'default))
+(add-hook 'buffer-list-update-hook 'highlight-selected-window)
