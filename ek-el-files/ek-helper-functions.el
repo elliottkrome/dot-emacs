298f7fe668559279235678ba092fe94322c17fff
Elliott Krome <tuf78296@temple.edu>
2018-06-17 03:48:23 -0500

    org capture from mu4e working

--- ek-el-files/ek-helper-functions.el
+++ ek-el-files/ek-helper-functions.el
@@ -12,3 +12,9 @@ than having to call `add-to-list' multiple times."
     (let ((last (nthcdr (1- nth) list)))
       (setcdr last (cddr last))
       list)))
+
+(defun print-strings-in-a-list (list)
+  "Print each element of LIST on a line of its own."
+  (while list
+    (prin1 (concat (car list)))
+    (setq list (cdr list))))
