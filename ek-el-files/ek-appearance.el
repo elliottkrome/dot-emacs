2e3a34307f433592fa4f2ec397baeafcd97aa9f5
Elliott Krome <tuf78296@temple.edu>
2018-06-18 01:23:17 -0500

    added ek-comment-tags back in

--- ek-el-files/ek-appearance.el
+++ ek-el-files/ek-appearance.el
@@ -1,6 +1,7 @@
 (load "~/.emacs.d/ek-el-files/appearance/ek-mode-line.el")
 (load "~/.emacs.d/ek-el-files/appearance/ek-window-behavior.el")
 (load "~/.emacs.d/ek-el-files/appearance/ek-font-lock.el")
+(load "~/.emacs.d/ek-el-files/appearance/ek-comment-tags.el")
 (load "~/.emacs.d/ek-el-files/appearance/ek-hideshow.el")
 (load "~/.emacs.d/ek-el-files/appearance/ek-icons.el")
 (load "~/.emacs.d/ek-el-files/appearance/ek-font-general.el")
