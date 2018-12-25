a4b21f75bdc11f7a863611ff85d26b8c244507c1
Elliott Krome <tuf78296@temple.edu>
2018-12-24 23:12:36 -0700

    no use-package for org-drill

--- ek-el-files/org/ek-org-drill.el
+++ ek-el-files/org/ek-org-drill.el
@@ -1,20 +1,13 @@
-(use-package org-drill
-  :defer t
-  :commands (org-drill
-             org-drill-tree
-             org-drill-directory)
-  :init
-  (setq org-drill-maximum-items-per-session         50
-        org-drill-maximum-duration                  20   ; 20 minutes
-        org-drill-use-visible-cloze-face-p           t
-        org-drill-add-random-noise-to-intervals-p    t
-        org-drill-hint-separator                  "||"
-        org-drill-left-cloze-delimiter            "<["
-        org-drill-right-cloze-delimiter           "]>"
-        ; org-drill-learn-fraction                0.25
-        ; org-drill-cram-hours 2
-        ; org-drill-leech-method 'warn
-	)
-  :config
-  (progn
-    (add-to-list 'org-modules 'org-drill)))
+(require 'org-drill)
+
+(setq org-drill-maximum-items-per-session         50
+      org-drill-maximum-duration                  20   ; 20 minutes
+      org-drill-use-visible-cloze-face-p           t
+      org-drill-add-random-noise-to-intervals-p    t
+      org-drill-hint-separator                  "||"
+      org-drill-left-cloze-delimiter            "<["
+      org-drill-right-cloze-delimiter           "]>"
+      ;; org-drill-learn-fraction                0.25
+      ;; org-drill-cram-hours 2
+      ;; org-drill-leech-method 'warn
+      )
