f62f2259253c04a4d6e682e4e5520cd6edac09c9
Elliott Krome <tuf78296@temple.edu>
2019-02-16 17:20:58 -0600

    added bad face back to org links

--- ek-el-files/org/ek-org-gnrl-ui.el
+++ ek-el-files/org/ek-org-gnrl-ui.el
@@ -73,8 +73,8 @@
 
 ;; org mode will fontify bad links with `org-warning' face (on refontify)
 ;;
-;(org-link-set-parameters
-; "file"
-; :face (lambda (path) (if (file-exists-p path) 'org-link 'org-warning)))
+(org-link-set-parameters
+ "file"
+ :face (lambda (path) (if (file-exists-p path) 'org-link 'org-warning)))
 
 ;;; ek/org-gnrl-ui.el ends here
